8af72ff179d50e1b1c92e51a48a396d47d35c7b7
tg_huy <56349289+truonggiahuy227@users.noreply.github.com>
2024-04-05 14:55:18 +0700

    Update get-from-minio.sh

--- get-from-minio.sh
+++ get-from-minio.sh
@@ -43,7 +43,7 @@ OUT_FILE=$6
 
 # Static Vars
 DATE=$(date -R --utc)
-CONTENT_TYPE='application/zstd'
+CONTENT_TYPE='application/octet-stream'
 SIG_STRING="GET\n\n${CONTENT_TYPE}\n${DATE}\n${MINIO_PATH}"
 SIGNATURE=`echo -en ${SIG_STRING} | openssl sha1 -hmac ${PASSWORD} -binary | base64`
 PROTOCOL="http"
